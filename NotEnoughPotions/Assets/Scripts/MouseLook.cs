58e8bdda08bb03d21efe61ef882ad837139f75a5
SISI <schwermc@my.erau.edu>
2025-09-09 13:12:57 -0700

    Create basic movement : Create 1st person camera
    Create basic movement
    Create 1st person camera

---
+++ NotEnoughPotions/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensitivity = 1500f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY; // += cause the roations to be flipped
+        xRotation = Mathf.Clamp(xRotation, -45, 45);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
